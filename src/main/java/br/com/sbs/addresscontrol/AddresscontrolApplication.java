b0facfb8ae553248e9de18a0fea381bb2672be02
sergiobsilva2505 <sergio.bezerra@outlook.com>
2021-04-28 22:02:18 -0300

    init

---
+++ src/main/java/br/com/sbs/addresscontrol/AddresscontrolApplication.java
@@ -0,0 +1,13 @@
+package br.com.sbs.addresscontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddresscontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddresscontrolApplication.class, args);
+	}
+
+}
